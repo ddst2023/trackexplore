bc6d94dde346caa1ea300af0b2d2c27f73187f9d
ddltv <dehan@shoptoken.com>
2023-12-20 12:14:35 -0500

    Detect port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+  await app.listen( parseInt(process.env.PORT, 10) || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
